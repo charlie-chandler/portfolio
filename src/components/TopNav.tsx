72385ae2f7668722f19f227fdc29b90e21b6791d
cchand-vanc <cmchandler@live.co.uk>
2023-06-13 22:38:27 -0700

    Added styling for nav links

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -5,6 +5,7 @@ import "../styles/TopNav.css";
 function TopNav() {
 
   const topNavCharlie = classNames("nav-charlie");
+  const topNavChandler = classNames("nav-chandler");
   const topNavLinks = classNames("nav-links");
   const navAnchorClass = classNames("nav-anchors");
   const resumeButtonClass = classNames("nav-resume-btn");
@@ -12,13 +13,15 @@ function TopNav() {
 
   return (
     <nav>
-      <div className={topNavCharlie}>CHARLIE CHANDLER</div>
+      <div className={topNavCharlie}>CHARLIE</div>
+      <div className={topNavChandler}>CHANDLER</div>
+      
       <div className={topNavLinks} id="navLinks">
-      <a href="/about" className={navAnchorClass}>About</a>
-      <a href="/projects" className={navAnchorClass}>Projects</a>
-      <a href="/skills" className={navAnchorClass}>Skills</a>
-      <a href="/contact" className={navAnchorClass}>Contact</a>
-      <a href="https://flowcv.com/resume/uc0n7klepo" className={resumeButtonClass}>Resumé</a>
+        <a href="/about" className={navAnchorClass}>About</a>
+        <a href="/projects" className={navAnchorClass}>Projects</a>
+        <a href="/skills" className={navAnchorClass}>Skills</a>
+        <a href="/contact" className={navAnchorClass}>Contact</a>
+        <a href="https://flowcv.com/resume/uc0n7klepo" target="_blank" className={resumeButtonClass}>Resumé</a>
       </div>
     </nav>
   )
