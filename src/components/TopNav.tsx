1f311d5a23c2380073c2da15ae08a2df763c69cf
cchand-vanc <cmchandler@live.co.uk>
2023-06-13 18:31:22 -0700

    add styling for nav bar

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -4,18 +4,21 @@ import "../styles/TopNav.css";
 
 function TopNav() {
 
-  const topNavClass = classNames("top-nav");
+  const topNavCharlie = classNames("nav-charlie");
+  const topNavLinks = classNames("nav-links");
   const navAnchorClass = classNames("nav-anchors");
+  const resumeButtonClass = classNames("nav-resume-btn");
+
 
   return (
     <nav>
-      <div className={topNavClass}>CHARLIE CHANDLER</div>
-      <div>
+      <div className={topNavCharlie}>CHARLIE CHANDLER</div>
+      <div className={topNavLinks} id="navLinks">
       <a href="/about" className={navAnchorClass}>About</a>
       <a href="/projects" className={navAnchorClass}>Projects</a>
       <a href="/skills" className={navAnchorClass}>Skills</a>
       <a href="/contact" className={navAnchorClass}>Contact</a>
-      <a href="https://flowcv.com/resume/uc0n7klepo" className="navAnchorClass">Resumé</a>
+      <a href="https://flowcv.com/resume/uc0n7klepo" className={resumeButtonClass}>Resumé</a>
       </div>
     </nav>
   )
