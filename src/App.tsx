06590ea5ed2c992b317dfde3e880d29e4d1da535
cchand-vanc <cmchandler@live.co.uk>
2023-06-14 17:22:46 -0700

    Create Introduction component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import TopNav from './components/TopNav';
+import Introduction from './components/Introduction';
 
 function App() {
   return (
     <div className="App">
       <TopNav/>
+      <Introduction/>
       <p>
       Rest of the app container will go here
       </p>
---
+++ src/components/Introduction.tsx
@@ -0,0 +1,34 @@
+import React from "react"
+import classNames from "classnames";
+import "../styles/Introduction.css";
+
+function Introduction() {
+
+  const introContainer = classNames('intro-container');
+  const introCopy = classNames('intro-copy');
+  const hiCharlie = classNames('hi-charlie');
+  const introPhoto = classNames('intro-photo');
+
+
+  return (
+    <div>
+      <div className={introContainer}>
+        <div className={introCopy}>
+          <div className={hiCharlie}>Hi I'm Charlie</div>
+          <p>
+            A full stack developer in Vancouver, BC.
+            <br />
+            I'm ambitious, curious and enthusiastic.
+            <br />
+            If I can offer any value to you - feel free to get in touch.
+          </p>
+        </div>
+        <div className={introPhoto}>
+          <img src="images/blob_and_photo2.png" alt="green-blob" className={introPhoto} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Introduction;
