ce9f2454b71ac1b2a4ad0ae03537fb82d7fd3ee5
cchand-vanc <cmchandler@live.co.uk>
2023-06-21 14:38:50 -0700

    Add separate Attribute boxes and add Attribute to App

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import TopNav from './components/TopNav';
 import Introduction from './components/Introduction';
 import Timeline from './components/Timeline';
+import Attributes from './components/Attributes';
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <TopNav/>
       <Introduction/>
       <Timeline/>
+      <Attributes/>
       <p>
       Rest of the app container will go here
       </p>
--- src/components/Attributes.tsx
+++ src/components/Attributes.tsx
@@ -1,7 +1,36 @@
+import React from "react"
+import classNames from "classnames";
+import "../styles/Attributes.css";
+
 function Attributes () {
+
+  const background = classNames('attribute-background');
+  const title = classNames('title');
+
+  const attributeContainer = classNames('attribute-container');
+  const attributeBox = classNames('attribute-box');
+
   return (
-    <div>
-      
+    <div className={background}>
+      <h1 className={title}>My Strengths</h1>
+      <div className={attributeContainer}>
+
+        <div className={attributeBox}>
+          <h2>Attribute 1</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adispsicng elit, sed do eiusmod tempo</p>
+        </div>
+        
+        <div className={attributeBox}>
+          <h2>Attribute 2</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adispsicng elit, sed do eiusmod tempo</p>
+        </div>
+
+        <div className={attributeBox}>
+          <h2>Attribute 3</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adispsicng elit, sed do eiusmod tempo</p>
+        </div>
+
+      </div>
     </div>
   )
 }
