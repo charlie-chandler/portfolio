52fb97a6812c0889b1bb9a9c2fea5d928ece5b1f
cchand-vanc <cmchandler@live.co.uk>
2023-06-30 11:59:32 -0700

    Remove image description from SliderWindow

--- src/components/SliderWindow.tsx
+++ src/components/SliderWindow.tsx
@@ -1,43 +1,24 @@
-import Mojo from "images/mojo_panel.png";
-import Scheduler from "../images/scheduler_panel.png";
-import Jungle from "../images/jungle_panel.png";
-import Tweeter from "../images/tweeter_panel.png";
-import classNames from "classnames";
+import Mojo from "../assets/mojo_panel.png"; 
+import Scheduler from "../assets/scheduler_panel.png";
+import Jungle from "../assets/jungle_panel.png";
+import Tweeter from "../assets/tweeter_panel.png";
+
 
 export default [
   {
     title: "Mojo",
-    description: "This is the first slider Image of our carousel",
     urls: Mojo,
   },
   {
     title: "Scheduler",
-    description: "This is the second slider Image of our carousel",
     urls: Scheduler,
   },
   {
     title: "Jungle",
-    description: "This is the third slider Image of our carousel",
     urls: Jungle,
   },
   {
     title: "Tweeter",
-    description: "This is the fourth slider Image of our carousel",
     urls: Tweeter,
   },
-  // {
-  //   title: "Fifth Slide",
-  //   description: "This is the Fifth slider Image of our carousel",
-  //   urls: "https://mdbootstrap.com/img/Photos/Slides/img%20(147).jpg",
-  // },
-  // {
-  //   title: "Sixth Slide",
-  //   description: "This is the Sixth slider Image of our carousel",
-  //   urls: "https://mdbootstrap.com/img/Photos/Slides/img%20(105).jpg",
-  // },
-  // {
-  //   title: "Seventh Slide",
-  //   description: "This is the Seventh slider Image of our carousel",
-  //   urls: "https://mdbootstrap.com/img/Photos/Slides/img%20(102).jpg",
-  // },
 ];
