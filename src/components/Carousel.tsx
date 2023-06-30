6b1d192b192ae84d7e939b1ca09a8620eddb77cf
cchand-vanc <cmchandler@live.co.uk>
2023-06-29 17:54:21 -0700

    Rename Projects to Carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,24 +1,54 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import classNames from "classnames";
-import "../styles/Projects.css";
+
+import "../styles/Carousel.css";
+import "./slider.css";
+import SliderContent from "./SliderContent";
+import Dots from "./Dots";
+import Arrows from "./Arrows";
+import SliderWindow from "./SliderWindow";
+
 // import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
 // import '@splidejs/react-splide/css/core';
 
-function Projects() {
+const len = SliderWindow.length - 1;
+
+function Carousel(props: any) {
 
   const background = classNames('projects-background');
   const title = classNames('title');
-  // const carousel = classNames('carousel splide');
-  // const track = classNames('splide__track');
-  // const list = classNames('splide__list');
   const slide = classNames('splide__slide');
   const wrapper = classNames('custom-wrapper');
 
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveIndex(activeIndex === len ? 0 : activeIndex + 1);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [activeIndex]);
 
   return (
     <div className={background}>
       <h1 className={title}>My Projects</h1>
       <p>Click on any project for more details</p>
+      <div className="slider-container">
+        <SliderContent activeIndex={activeIndex} sliderImage={SliderWindow} />
+        <Arrows
+          prevSlide={() =>
+            setActiveIndex(activeIndex < 1 ? len : activeIndex - 1)
+          }
+          nextSlide={() =>
+            setActiveIndex(activeIndex === len ? 0 : activeIndex + 1)
+          }
+        />
+        <Dots
+          activeIndex={activeIndex}
+          sliderImage={SliderWindow}
+          onclick={(activeIndex: any) => setActiveIndex(activeIndex)}
+        />
+      </div>
 
       {/* <Splide aria-label="My Carousel" hasTrack={false}>
         <div className={wrapper}>
@@ -39,9 +69,9 @@ function Projects() {
         </div>
       </Splide> */}
 
-      
+
     </div>
   )
 }
 
-export default Projects;
+export default Carousel;
