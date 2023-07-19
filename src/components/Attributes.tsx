549c35209305581669e1c50cf4b07653d32a6703
cchand-vanc <cmchandler@live.co.uk>
2023-07-18 18:42:01 -0700

    Attributes boxes border styling

--- src/components/Attributes.tsx
+++ src/components/Attributes.tsx
@@ -13,16 +13,19 @@ function Attributes() {
 
   return (
     <div className={background}>
-      <h1 className={title}>My Strengths</h1>
+      <h1 className={title}>What I Bring To My Work</h1>
       <div className={attributeContainer}>
 
         <div className={attributeBox}>
           <h2>Detail oriented</h2>
-          <p>I have a keen eye for detail so I am able to meticulously review code, understand the nuances of project requirements, and test/debug to resolve technical issues of varying difficulty.</p>
+          <p>Nothing makes me happier than dotting the i’s and crossing the t’s.
+            <br />
+            <br />
+            I have a keen eye for detail that translates well to writing and testing code, as well as understanding project requirements and debugging those nitty-gritty problems.</p>
         </div>
 
         <div className={attributeBox}>
-          <h2>User-centric approach</h2>
+          <h2>Unique user-centric approach</h2>
           <p>My background in client-facing roles has given me great insight into the user's experience and perspective which I can apply to my coding.</p>
         </div>
 
