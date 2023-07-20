0359451ff4cef85f59a46f6a0dc6fa335d3bc6ca
cchand-vanc <cmchandler@live.co.uk>
2023-07-20 15:05:33 -0700

    Remove parallax

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
-
 
 import TopNav from './components/TopNav';
 import Introduction from './components/Introduction';
@@ -15,75 +13,12 @@ function App() {
   return (
     <div className="App">
       <TopNav />
-      <Parallax pages={5}>
-        <ParallaxLayer
-        offset={0}
-        speed={1}
-        factor={0}
-        sticky={{ start: 0}}
-        style={{
-          backgroundImage: ``,
-          backgroundSize: 'cover',
-        }}>
-          <Introduction />
-        </ParallaxLayer>
-
-        <ParallaxLayer
-          offset={1}
-          speed={1}
-          factor={2}
-          style={{
-            backgroundImage: ``,
-            backgroundSize: 'cover',
-          }}>
-          <Timeline />
-        </ParallaxLayer>
-
-        <ParallaxLayer
-        offset={2}
-        speed={1}
-        factor={2}
-        style={{
-          backgroundImage: ``,
-          backgroundSize: 'cover',
-        }}>
-          <Attributes />
-        </ParallaxLayer>
-
-        <ParallaxLayer
-        offset={3}
-        speed={1}
-        factor={2}
-        style={{
-          backgroundImage: ``,
-          backgroundSize: 'cover',
-        }}>
-          <Carousel />
-        </ParallaxLayer>
-
-        <ParallaxLayer
-        offset={4}
-        speed={1}
-        factor={2}
-        style={{
-          backgroundImage: ``,
-          backgroundSize: 'cover',
-        }}>
-          <Skills />
-        </ParallaxLayer>
-
-        <ParallaxLayer
-          offset={5}
-          speed={0.5}
-          factor={3}
-          style={{
-            backgroundImage: '/images/green_blob',
-            backgroundSize: 'cover',
-          }}
-        >
-          <Contact />
-        </ParallaxLayer>
-      </Parallax>
+      <Introduction />
+      <Timeline />
+      <Attributes />
+      <Carousel />
+      <Skills />
+      <Contact />
     </div>
   );
 }
