6c9b6f544cf020b49682f9c3eeec5f2cf5159b75
cchand-vanc <cmchandler@live.co.uk>
2023-07-26 17:36:32 -0700

    Add Link components to nav bar

--- src/components/home-page/TopNav.tsx
+++ src/components/home-page/TopNav.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import classNames from "classnames";
 import "../../styles/TopNav.css";
+import { Link } from "react-router-dom";
 
 function TopNav() {
 
@@ -13,15 +14,15 @@ function TopNav() {
 
   return (
     <nav>
-      <a href="#top">
+      <Link to="/#top">
         <div className={topNavCharlie} >CHARLIE</div>
         <div className={topNavChandler}>CHANDLER</div>
-      </a>
+      </Link>
       <div className={topNavLinks} id="navLinks">
-        <a href="#about" className={navAnchorClass}>About</a>
-        <a href="#projects" className={navAnchorClass}>Projects</a>
-        <a href="#skills" className={navAnchorClass}>Skills</a>
-        <a href="#contact" className={navAnchorClass}>Contact</a>
+        <Link to="/#about" className={navAnchorClass}>About</Link>
+        <Link to="/#projects" className={navAnchorClass}>Projects</Link>
+        <Link to="/#skills" className={navAnchorClass}>Skills</Link>
+        <Link to="/#contact" className={navAnchorClass}>Contact</Link>
         <a href="https://drive.google.com/file/d/19CjSNIQ9QFCpoGkjMhpbP_tag8le6yti/view?usp=share_link" target="_blank" rel="noreferrer" className={resumeButtonClass}>Resumé</a>
       </div>
     </nav>
