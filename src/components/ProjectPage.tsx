7c54fe1e39e2bb717ce095dbbc0a3a4ca99d2d4a
cchand-vanc <cmchandler@live.co.uk>
2023-07-27 10:00:01 -0700

    Create ProjectPage component and type interface

---
+++ src/components/ProjectPage.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import classNames from "classnames";
+import TopNav from "./TopNav";
+import { Link } from "react-router-dom";
+
+
+interface ProjectPageProps {
+  projectTitle: string;
+  stack: [];
+  features: [];
+  image: HTMLImageElement;
+  repoLink: string;
+}
+const ProjectPage: React.FC<ProjectPageProps> = ({
+  projectTitle,
+  stack,
+  features,
+  image,
+  repoLink,
+}) => {
+
+  const background = classNames("mojo-background");
+  const title = classNames('title');
+  const subtitle = classNames('subtitle');
+  const mojoContainer = classNames('mojo-container');
+  const leftContainer = classNames('left-mojo');
+  const rightContainer = classNames('right-mojo');
+  const stackList = classNames('stack-list');
+  const featureList = classNames('feature-list');
+
+
+  return (
+    <>
+      <TopNav />
+      <div className={background}>
+        <h1 className={title}>{projectTitle}</h1>
+        <h3 className={subtitle}>Mood Journal Web App</h3>
+        <div className={mojoContainer}>
+          <div className={leftContainer}>
+            <div className={stackList}></div>
+            <div className={featureList}></div>
+            <Link to="/#top"><button>Back to home</button></Link>
+          </div>
+          <div className={rightContainer}>
+            <img src="src/assets/mojo_demo.gif" alt="mojo demo gif" />
+            <a href="https://github.com/charlie-chandler/MoJo"><button>Repo</button></a>
+          </div>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default ProjectPage;
