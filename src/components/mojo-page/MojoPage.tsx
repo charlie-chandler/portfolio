b9f80d35f09b8dff863b509e6e7ed550c49d839c
cchand-vanc <cmchandler@live.co.uk>
2023-07-26 17:33:17 -0700

    Add mojo page into router and build out its component

--- src/components/mojo-page/MojoPage.tsx
+++ src/components/mojo-page/MojoPage.tsx
@@ -1,15 +1,41 @@
 import React from "react";
-
+import classNames from "classnames";
 import TopNav from "../home-page/TopNav";
+import { Link } from "react-router-dom";
 
 
 function MojoPage() {
+
+  const background = classNames("mojo-background");
+  const title = classNames('title');
+  const subtitle = classNames('subtitle');
+  const mojoContainer = classNames('mojo-container');
+  const leftContainer = classNames('left-mojo');
+  const rightContainer = classNames('right-mojo');
+  const stack = classNames('stack');
+  const featureList = classNames('feature-list');
+
+
+
   return (
     <>
-    <TopNav/>
-    <div>
+      <TopNav />
+      <div className={background}>
+        <h1 className={title} id="skills">Mojo</h1>
+        <h3 className={subtitle}>Mood Journal Web App</h3>
+        <div className={mojoContainer}>
+          <div className={leftContainer}>
+            <div className={stack}></div>
+            <div className={featureList}></div>
+            <Link to="/#top"><button>Back to home</button></Link>
+          </div>
+          <div className={rightContainer}>
+            <img src="src/assets/mojo_demo.gif" alt="mojo demo gif" />
+            <a href="https://github.com/charlie-chandler/MoJo"><button>Repo</button></a>
+          </div>
 
-    </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MojoPage from './components/mojo-page/MojoPage';
 
 
 const router = createBrowserRouter([
@@ -14,6 +12,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <App/>,
   },
+  {
+    path: "mojo",
+    element: <MojoPage/>,
+  }
 ]);
 
 
