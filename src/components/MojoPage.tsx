5fffff5945d8329a5ce6e235b05f3375e82d1920
cchand-vanc <cmchandler@live.co.uk>
2023-07-28 13:27:54 -0700

    Add new stack to MojoPage

--- src/components/MojoPage.tsx
+++ src/components/MojoPage.tsx
@@ -8,11 +8,12 @@ const MojoPage: React.FC = () => {
     <ProjectPage
        projectTitle="Mojo"
        projectSubtitle="Wellness web app" 
-       stack={["JavaScript", "React", "Bootstrap", "Express", "Node.js", "SQL", "Jest"]}
+       stack={["JavaScript", "React", "Bootstrap", "Express", "Node.js", "SQL", "Jest", "Chartjs"]}
        features={["Mood selector", "Journal", "Customizable daily wellness checklist", "Calendar API", "Spotify playlist suggestions that compliment your mood"]}
        image='images/project-reel/mojo_home.png'
        repoLink="https://github.com/charlie-chandler/MoJo"
     />
+
   </>
   )
   
