468ba3128c91c77d248e9ef858458ac5d1e2fbc6
cchand-vanc <cmchandler@live.co.uk>
2023-07-28 20:36:11 -0700

    Add overflow styling to accomodate changing div sizes in Projects

--- src/components/MojoPage.tsx
+++ src/components/MojoPage.tsx
@@ -15,7 +15,7 @@ const MojoPage: React.FC = () => {
        image3='images/project-reel/mojo_moodscape.gif'
        challenge1="This project used several API intergrations to make it work, so our team's biggest challenge was understanding external libraries and version compatibility wit React."
        challenge2="We were able to overcome this with a large amount of research and pair programming through the toughest problems."
-       goal1="To create a wellness app to check in with your mood and daily actions through a journal, and building awareness of your mood. "
+       goal1="To create a wellness app to check in with your mood and daily actions through journal, building awareness of your actions, and data. By way of the Spotify API, Mojo will also make a playlist recommendation for music that compliments your current mood."
        goal2="The app has gentle aesthetic features, blurred gradients, and soft lines to create a harmonious experience for the user."
        repoLink="https://github.com/charlie-chandler/MoJo"
     />
