3a50efa7f2a46066c89b61423804ef5b205adcc6
cchand-vanc <cmchandler@live.co.uk>
2023-07-31 16:46:57 -0700

    Add copy to SchedulerPage

--- src/components/SchedulerPage.tsx
+++ src/components/SchedulerPage.tsx
@@ -13,10 +13,10 @@ const SchedulerPage: React.FC = () => {
        image1='images/project-reel/scheduler_demo.gif'
        image2='images/project-reel/scheduler_edit.png'
        image3='images/project-reel/scheduler_new.png'
-       challenge1="challenge1"
-       challenge2="challenge2"
-       goal1="goal1"
-       goal2="goal2"
+       challenge1="Stale states and shakey custom hooks caused a bit of a headache while building Scheduler."
+       challenge2="This reinforced the importance of diligent management of states."
+       goal1="The goal with Scheduler was to dive into building custom hooks, and focus on using Test-Driven Development to inform the progress of the app. "
+       goal2="Using Storybook for the first time was a key tool, allowing me to test each component in isolation as I built them."
        repoLink="https://github.com/charlie-chandler/scheduler"
     />
 
