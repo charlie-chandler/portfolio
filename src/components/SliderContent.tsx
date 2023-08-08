b1487f2855785508feec589ee9b46e7aa1eccdd3
cchand-vanc <cmchandler@live.co.uk>
2023-08-08 16:57:26 -0700

    Swap out Link component for anchor tag in SliderContent to fix page loading location bug

--- src/components/SliderContent.tsx
+++ src/components/SliderContent.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 function SliderContent({ activeIndex, sliderImage }: any) {
   return (
@@ -9,10 +8,10 @@ function SliderContent({ activeIndex, sliderImage }: any) {
           key={index}
           className={index === activeIndex ? "slides active" : "inactive"}
         >
-          <Link to={slide.route}>
+            <a href={slide.route}>
           <img className="slide-image" src={slide.urls} alt="project preview" />
           <p className="slide-title">{slide.title}</p>
-          </Link>
+          </a>
         </div>
       ))}
     </section>
